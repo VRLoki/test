bfe09fa2bfafeca922998880389704e77a3d8d64
Vincent Roessle <vincent.roessle@gmail.com>
2024-09-04 16:48:01 +0200

    adding mongodb-server to base image

--- 2373/10-count_shows_by_genre.sql
+++ 2373/10-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
--- Write a script that lists all genres from hbtn_0d_tvshows and displays the
--- number of shows linked to each.
-SELECT t1.name AS genre, count(t2.show_id) AS number_of_shows
-FROM tv_genres t1
-INNER JOIN tv_show_genres t2
-ON t1.id = t2.genre_id
-GROUP BY t1.id
-ORDER BY count(t2.show_id)
+-- number of shows by genre
+SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
+FROM tv_show_genres AS tsg
+JOIN tv_genres AS tg
+ON tg.id = tsg.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
