df829e6fdba72c7d97533d5284ef0de4f733e1e3
Vincent Roessle <vincent.roessle@gmail.com>
2024-09-04 14:26:15 +0200

    adding nodeJS Basics

---
+++ 2224/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine($"{new StringBuilder(str.Length * 3).Insert(0, str, 3)}");
+        Console.WriteLine($"{str.Substring(0, 9)}");
+    }
+}
