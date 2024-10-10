0659a19a5836b4f9488a91fa414989c3e0157d2e
Vincent Roessle <vincent.roessle@gmail.com>
2024-10-10 10:24:35 -0300

    3111

--- Heranca/forma/FormaGeometrica.java
+++
@@ -1,5 +0,0 @@
-public class FormaGeometrica {
-    public double area() throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Funcao area nao implementada");
-    }
-}
